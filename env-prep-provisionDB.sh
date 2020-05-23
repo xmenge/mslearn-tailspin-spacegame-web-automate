e754e688d13badb0836abcd9ca9889e6b609e2ee
gl <39395994+xmenge@users.noreply.github.com>
2020-05-23 21:33:53 +1000

    Add provisioning template

---
+++ env-prep-provisionDB.sh
@@ -0,0 +1,22 @@
+UNIQUE_ID=3008
+
+az keyvault create \
+  --name tailspin-vault-$UNIQUE_ID \
+  --resource-group tsg-web-rg \
+  --enabled-for-template-deployment true
+
+az keyvault show --name tailspin-vault-$UNIQUE_ID --query id --output tsv
+
+SQL_PASSWORD=$(openssl rand -base64 32)
+
+az keyvault secret set \
+  --vault-name tailspin-vault-$UNIQUE_ID \
+  --name "adminPassword" \
+  --value "$SQL_PASSWORD"
+
+az keyvault secret set \
+  --vault-name tailspin-vault-$UNIQUE_ID \
+  --name "firstRun" \
+  --value "true"
+
+  
